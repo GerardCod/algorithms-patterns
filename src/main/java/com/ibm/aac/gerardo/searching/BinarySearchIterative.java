9e47b4bece7474ad3848e95b8639f6c383efbe54
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-10-15 09:48:17 -0500

    modify: BinarySearchIterative class created

--- src/main/java/com/ibm/aac/gerardo/searching/BinarySearchIterative.java
+++ src/main/java/com/ibm/aac/gerardo/searching/BinarySearchIterative.java
@@ -1,5 +1,28 @@
 package com.ibm.aac.gerardo.searching;
 
-public class BinarySearchIterative {
+public class BinarySearchIterative implements Searchable<String> {
+
+  @Override
+  public boolean search(String[] data, String target) {
+    boolean found = false;
+    int min = 0;
+    int max = data.length - 1;
+
+    while (!found && min <= max) {
+      int middle = (min + max) / 2;
+
+      if (target.compareTo(data[middle]) < 0) {
+        max = middle - 1;
+      } else if (target.compareTo(data[middle]) > 0) {
+        min = middle + 1;
+      } else {
+        found = true;
+      }
+
+    }
+
+
+    return found;
+  }
   
 }
