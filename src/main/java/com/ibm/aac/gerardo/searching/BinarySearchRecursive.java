984780ca8109afc5fa5b76fb1fa291a1e91f3f8f
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-10-15 09:55:22 -0500

    modify: Searchable interface implemented in BinarySearchRecursive class

--- src/main/java/com/ibm/aac/gerardo/searching/BinarySearchRecursive.java
+++ src/main/java/com/ibm/aac/gerardo/searching/BinarySearchRecursive.java
@@ -1,5 +1,27 @@
 package com.ibm.aac.gerardo.searching;
 
-public class BinarySearchRecursive {
+public class BinarySearchRecursive implements Searchable<String> {
+
+  @Override
+  public boolean search(String[] data, String target) {
+    return searchHelper(data, target, 0, data.length - 1);
+  }
   
+  private boolean searchHelper(String[] data, String target, int min, int max) {
+    if (min > max) {
+      return false;
+    }
+
+    int middle = (min + max) / 2;
+
+    if (target.compareTo(data[middle]) < 0) {
+      return searchHelper(data, target, min, middle - 1);
+    }
+
+    if (target.compareTo(data[middle]) > 0) {
+      return searchHelper(data, target, middle + 1, max);
+    }
+
+    return true;
+  }
 }
